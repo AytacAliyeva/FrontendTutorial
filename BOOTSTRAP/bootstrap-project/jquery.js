e8ad26b2aca961c3eb400e6ccea4b8f98ca988cd
AytacAliyeva <aeliyeva15@std.beu.edu.az>
2020-07-01 12:24:48 +0400

    js

--- BOOTSTRAP/bootstrap-project/jquery.js
+++ BOOTSTRAP/bootstrap-project/jquery.js
@@ -2,13 +2,13 @@ $('.owl-carousel').owlCarousel({
     loop:true,
     margin:30,
     dots:false,
-    nav:true,
+    nav:false,
     smartSpeed: 2500,
     autoplay: 2500,
     autoplayTimeout: 3000,
     autoplayHoverPause: true,
-    navText : ["<div class='blog-slider__leftArrow'><img src='img/home/left-arrow.png'></div>",
-        "<div class='blog-slider__rightArrow'><img src='img/home/right-arrow.png'></div>"],
+/*    navText : ["<div class='blog-slider__leftArrow'><img src='img/home/left-arrow.png'></div>",
+        "<div class='blog-slider__rightArrow'><img src='img/home/right-arrow.png'></div>"],*/
     responsive:{
         0:{
             items:1
---
+++ JAVASCRIPT/javascript-output/javascript.js
@@ -0,0 +1,11 @@
+function function_name() {
+   /* document.getElementById("java").style.color='red';*/
+
+    /*document.write(9+6); bundan evvelkiler silir*/
+
+    /*window.alert('Tesdiqleyirsinizmi?');*/
+
+   /* console.log(9+6);*/
+
+}
+
